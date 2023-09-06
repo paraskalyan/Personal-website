293b9aab94d034b848334d957e5414dd4a3dcf87
Paras Kalyan <98208059+paraskalyan@users.noreply.github.com>
2023-09-06 16:23:41 +0530

    Update Main.jsx

--- src/Components/Main.jsx
+++ src/Components/Main.jsx
@@ -4,15 +4,19 @@ import Projects from './Projects'
 import About from './About'
 import Skills from './Skills'
 import Contact from './Contact'
-
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Footer from './Footer'
+import MobileMenu from './MobileMenu'
 const Main = () => {
   return (
-    <div className='w-[90%] ml-[10%] bg-[#1c1c1c] text-white min-h-full'>
+    <div className='w-[90%] ml-[10%] bg-[#1c1c1c] text-white min-h-full max-sm:w-full max-sm:ml-0'>
+        <MobileMenu/>
         <Home/>
         <Projects/>
         <About/>
         <Skills/>
         <Contact/>
+        <Footer/>
     </div>
   )
 }
